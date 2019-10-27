d06f68be07e18b95ca4d20ad712247523fd678fe
Jedrzej Nowacki <contact@jnowacki.pl>
2019-10-27 12:49:19 +0100

    Hamcrest matcher

--- src/test/java/pl/jnowacki/HamcrestExercisesTest.java
+++ src/test/java/pl/jnowacki/HamcrestExercisesTest.java
@@ -1,6 +1,11 @@
 package pl.jnowacki;
 
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -10,5 +15,59 @@ public class HamcrestExercisesTest {
     @Test
     public void fiveShouldBeGreaterThanThree() {
         assertThat(5, is(greaterThan(3)));
+    }
+
+    @Test
+    public void emptyStringShouldBeEmpty() {
+        assertThat("", isEmptyOrNullString());
+    }
+
+    @Test
+    public void doesStringContainKonfabulacja() {
+
+        String value = "afdsfdsfdsfaqwtjjfvvjfyu";
+
+        assertThat(value, not(containsString("konfabulacja")));
+    }
+
+    @Test
+    public void doesArrayContainRightValues() {
+
+        Integer[] values = {5, 7, 2};
+
+        assertThat(values, arrayContaining(5, 7, 2));
+    }
+
+    @Test
+    public void doesArrayContainRightValuesInAnyOrder() {
+
+        Integer[] values = {5, 7, 2};
+
+        assertThat(values, arrayContainingInAnyOrder(2, 5, 7));
+    }
+
+    @Test
+    public void doesListContainAndHasRightLength() {
+
+        List<String> values = Arrays.asList("raz", "dwa", "trzy");
+
+        assertThat(values, hasItem("dwa"));
+        assertThat(values, hasSize(3));
+    }
+
+    @Test
+    public void shouldBeginWithA() {
+        String testedValue = "aString";
+
+        assertThat(testedValue, beginsWith(is('a')));
+    }
+
+    private static Matcher<String> beginsWith(Matcher<? super Character> matcher) {
+        return new FeatureMatcher<String, Character>(matcher, "a String that begins with", "first letter") {
+            @Override
+            protected Character featureValueOf(String actual) {
+                return actual.charAt(0);
+            }
+        };
     }
 }
